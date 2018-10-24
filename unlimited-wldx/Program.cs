f96ebd0a6156e8999c8ca3dc1283bfdfc41866c1
starvii <starvii@gmail.com>
2018-10-25 01:47:01 +0800

    添加了文件日志功能

--- unlimited-wldx/Program.cs
+++ unlimited-wldx/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
-namespace unlimited_wldx
+namespace wldx
 {
 	static class Program
 	{
+		static private StreamWriter f = null;
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
+			f = new StreamWriter(new FileStream("log.txt", FileMode.Append));
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FromMain());
+			f.Close();
         }
 
 		/// <summary>
@@ -22,8 +27,8 @@ namespace unlimited_wldx
 		/// <param name="text"></param>
 		static public void Log(string text)
 		{
-			// TODO: aaaa
-			throw new NotImplementedException();
+			f.WriteLine(text);
+			f.Flush();
 		}
     }
 }
