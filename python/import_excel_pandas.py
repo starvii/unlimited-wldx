ad8ad067fdd07b26f756d672dc13b7de8861f822
starvii <starvii@mail.com>
2023-06-26 08:27:21 +0800

    doing import excel with pandas

---
+++ python/import_excel_pandas.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from typing import List, Tuple
+import pandas as pd
+import json
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
+
